1eb23661fabea272621f444402b67f3942d03529
Nguyhithatgud <nguyh432@gmail.com>
2026-04-18 15:36:28 +0700

    add: loading bg  follow theme selection

--- app/Loading.tsx
+++ app/Loading.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { useUISlice } from "@/stores/UI/useUIStore";
 const Loading = () => {
+  const { hotelTheme } = useUISlice();
   return (
     <>
-      <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+      <div
+        className={
+          hotelTheme === "dark"
+            ? "fixed inset-0 flex items-center justify-center bg-gray-800 z-50"
+            : "fixed inset-0 flex items-center justify-center bg-white z-50"
+        }
+      >
         <div className="text-center">
           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-amber-400 mx-auto mb-4"></div>
         </div>
